220cce380108efc171307a561dd8767104a6e5d7
Nadeem Ramjan <nadeemramjan@gmail.com>
2015-10-13 15:43:02 -0400

    Delete RefreshAllPivotTables.vb

--- RefreshAllPivotTables.vb
+++
@@ -1,9 +0,0 @@
-Private Sub RefreshAllPivotTables()
-  Dim w As Worksheet, p As PivotTable
-        For Each w In ThisWorkbook.Worksheets
-            For Each p In w.PivotTables
-            p.RefreshTable
-            p.Update
-        Next
-    Next
-End Sub
